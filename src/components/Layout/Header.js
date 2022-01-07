4110626786ea95a1035ed85ba903b4ec2e865bdf
abrunello <brunello@bludata.com>
2022-01-07 09:01:32 +0100

    creating header anc cart button component

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import styles from "./Header.module.css";
+import mealsImage from "../../assets/meals.jpeg";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton/>
+      </header>
+      <div className={styles["main-image"]}>
+        <img src={mealsImage} />
+      </div>
+    </>
+  );
+};
+
+export default Header;
