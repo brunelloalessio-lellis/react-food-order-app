b9b20fc244d7cb502853ba8bfc2997e9eedf38a6
abrunello <brunello@bludata.com>
2022-01-07 09:10:57 +0100

    creating meal list

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import AvaiableMeals from "./AvaiableMeals";
+import MealsSummary from "./MealsSummary";
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSummary />
+      <AvaiableMeals />
+    </>
+  );
+};
+
+export default Meals;
